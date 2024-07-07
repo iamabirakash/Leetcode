62d865e4dae57343d7807a7585404931c2735601
user.email like: "abir030204@gmail.com <abir030204@gmail.com>
2024-07-07 21:33:33 +0530

    1ms Solution

---
+++ GCD_Strings/GCD_strings.java
@@ -0,0 +1,22 @@
+package GCD_Strings;
+
+public class GCD_strings {
+    public static String gcdOfStrings(String str1, String str2) {
+        if(str2.length()>str1.length()){
+            return gcdOfStrings(str2,str1);
+        }
+        if(str1.equals(str2)){
+            return str1;
+        }
+        if(str1.startsWith(str2)){
+            return gcdOfStrings(str1.substring(str2.length()),str2);
+        }
+        return "";
+    }
+
+    public static void main(String[] args) {
+        String str1 = "ABCABC";
+        String str2 = "ABC";
+        System.out.println(gcdOfStrings(str1, str2)); // Output: "ABC"
+    }
+}
