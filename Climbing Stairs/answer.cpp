adf49c5ce08a95089a0ab246012505b90d7ca91c
Abir Akash Bora <143283517+iamabirakash@users.noreply.github.com>
2025-07-21 14:35:43 +0530

    21th July

---
+++ Climbing Stairs/answer.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int cnt = 0;
+    int climbStairs(int n) {
+        vector<int> fibo(n+1,-1);
+        fibo[0] = 1;
+        fibo[1] = 1;
+        for(int i=2;i<=n;i++){
+            fibo[i] = fibo[i-1]+fibo[i-2];
+        }
+        return fibo[n];
+    }
+};
