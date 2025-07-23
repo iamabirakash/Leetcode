1901d6575cfb74da165861859613e098cc1dab9f
Abir Akash Bora <143283517+iamabirakash@users.noreply.github.com>
2025-07-23 15:04:58 +0530

    23rd July

---
+++ Unique Paths/answer.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int path(int i,int j,int m,int n,vector<vector<int>> &dp){
+        if(i>=m || j>=n) return 0;
+        if(i==m-1 && j==n-1) return 1;
+        if(dp[i][j]!=-1) return dp[i][j];
+        int right = path(i,j+1,m,n,dp);
+        int down = path(i+1,j,m,n,dp);
+        return dp[i][j] = right+down;
+    }
+    int uniquePaths(int m, int n) {
+        vector<vector<int>> dp(m,vector<int>(n,-1));
+        return path(0,0,m,n,dp);
+    }
+};
