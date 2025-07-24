6c1f48fd422e6fbb768a5bee8c0cbba37bd928c9
Abir Akash Bora <143283517+iamabirakash@users.noreply.github.com>
2025-07-24 11:02:53 +0530

    24th July

---
+++ Assign Cookies/answer.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int findContentChildren(vector<int>& g, vector<int>& s) {
+        int cnt = 0;
+        sort(g.begin(),g.end());
+        sort(s.begin(),s.end());
+        int n = g.size();
+        int m = s.size();
+        int i=0;
+        int j=0;
+        while(i<n && j<m){
+            if(s[j]>=g[i]){
+                i++;
+                j++;
+                cnt++;
+            } else {
+                j++;
+            }
+        }
+        return cnt;
+    }
+};
