177424b7dbda0bda2a4d15429f9d8f4c2787076c
Abir Akash Bora <143283517+iamabirakash@users.noreply.github.com>
2026-01-21 22:16:14 +0530

    Added Fibonacci Number

---
+++ Fibonacci Number/ans.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int solve(int n, vector<int>& dp) {
+        if (n <= 1) return n;
+        if (dp[n] != 0) return dp[n];
+        dp[n] = solve(n - 1, dp) + solve(n - 2, dp);
+        return dp[n];
+    }
+    int fib(int n) {
+        vector<int> dp(n + 1,0);
+        return solve(n, dp);
+    }
+};
