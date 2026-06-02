18e01879980be117f52d7cb16c565062b87e6c09
Abir Akash Bora <143283517+iamabirakash@users.noreply.github.com>
2026-06-02 20:55:56 +0530

    added solution for earliest-finish-time-for-land-and-water-rides-i

---
+++ earliest-finish-time-for-land-and-water-rides-i/solution.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int earliestFinishTime(vector<int>& landStartTime,
+                           vector<int>& landDuration,
+                           vector<int>& waterStartTime,
+                           vector<int>& waterDuration) {
+        
+        int minL = 3000, minW = 3000, res = 3000;
+        int n = landStartTime.size(), m = waterStartTime.size();
+
+        for (int i = 0; i < n; i++) {
+            minL = min(minL, landStartTime[i] + landDuration[i]);
+        }
+
+        for (int i = 0; i < m; i++) {
+            minW = min(minW, waterStartTime[i] + waterDuration[i]);
+            res = min(res,
+                      max(minL, waterStartTime[i]) + waterDuration[i]);
+        }
+
+        for (int i = 0; i < n; i++) {
+            res = min(res,
+                      max(minW, landStartTime[i]) + landDuration[i]);
+        }
+
+        return res;
+    }
+};
