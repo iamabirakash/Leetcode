70930562e4295b144bbb8de6d8ebe1f752fb44f9
Abir Akash Bora <143283517+iamabirakash@users.noreply.github.com>
2026-06-23 11:55:25 +0530

    added solution for minimum-depth-of-binary-tree

--- minimum-depth-of-binary-tree/solution.cpp
+++ minimum-depth-of-binary-tree/solution.cpp
@@ -14,8 +14,8 @@ public:
     int minDepth(TreeNode* root) {
         if(root==nullptr) return 0;
         if(root->left==nullptr && root->right==nullptr) return 1;
-        if(root->left!=nullptr) return 1+minDepth(root->left);
-        if(root->right!=nullptr) return 1+minDepth(root->right);
+        if(root->right==nullptr) return 1+minDepth(root->left);
+        if(root->left==nullptr) return 1+minDepth(root->right);
         return 1+min(minDepth(root->left),minDepth(root->right));
     }
 };
