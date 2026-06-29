ae7ece40262365f6679c86a33d09a0b94b846339
Abir Akash Bora <143283517+iamabirakash@users.noreply.github.com>
2026-06-29 21:12:59 +0530

    added solution for maximum-population-year

---
+++ maximum-population-year/solution.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int maximumPopulation(vector<vector<int>>& logs) {
+
+        int pop[101] = {0};
+
+        for(auto &log : logs){
+            pop[log[0] - 1950]++;
+            pop[log[1] - 1950]--;
+        }
+
+        int maxPop = 0;
+        int year = 1950;
+        int curr = 0;
+
+        for(int i = 0; i < 101; i++){
+            curr += pop[i];
+
+            if(curr > maxPop){
+                maxPop = curr;
+                year = 1950 + i;
+            }
+        }
+
+        return year;
+    }
+};
