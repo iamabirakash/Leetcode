7ff7c5bd144136e31fc79ff47eeef79897fb4073
Abir Akash Bora <143283517+iamabirakash@users.noreply.github.com>
2026-06-30 15:58:46 +0530

    added solution for points-that-intersect-with-cars

---
+++ points-that-intersect-with-cars/solution.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int numberOfPoints(vector<vector<int>>& nums) {
+        int n = nums.size();
+        sort(nums.begin(), nums.end());
+        int ans = 0;
+        int start = nums[0][0];
+        int end = nums[0][1];
+        for(int i = 1; i < n; i++) {
+            if(nums[i][0] <= end) {
+                end = max(end, nums[i][1]);
+            } else {
+                ans += end-start+1;
+                start = nums[i][0];
+                end = nums[i][1];
+            }
+        }
+        ans += end-start+1;
+        return ans;
+    }
+};
