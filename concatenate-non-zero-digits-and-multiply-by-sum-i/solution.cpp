e2ae8caa8995ba843d8c9ae319be28873a464b5e
Abir Akash Bora <143283517+iamabirakash@users.noreply.github.com>
2026-07-07 15:08:13 +0530

    added solution for concatenate-non-zero-digits-and-multiply-by-sum-i

---
+++ concatenate-non-zero-digits-and-multiply-by-sum-i/solution.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    long long sumAndMultiply(int n) {
+         long long num = 0;
+        long long place = 1;
+        int sum = 0;
+
+        while (n > 0) {
+            int digit = n % 10;
+
+            if (digit != 0) {
+                num = digit * place + num;
+                place *= 10;
+                sum += digit;
+            }
+
+            n /= 10;
+        }
+
+        return num * sum;
+    }
+};
