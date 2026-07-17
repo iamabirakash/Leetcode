5eaa90bc502f54c62334c2cc25f268d2f25d9b81
Abir Akash Bora <143283517+iamabirakash@users.noreply.github.com>
2026-07-17 15:34:31 +0530

    added solution for first-missing-positive

--- first-missing-positive/solution.cpp
+++ first-missing-positive/solution.cpp
@@ -1,16 +1,17 @@
 class Solution {
 public:
     int firstMissingPositive(vector<int>& nums) {
-        nums.erase(remove_if(nums.begin(), nums.end(), [](int n) { return n <= 0; }), nums.end());
         sort(nums.begin(), nums.end());
+
         int target = 1;
         for (int n : nums) {
-            if (n == target) {
+            if (n > 0 && n == target) {
                 target++;
             } else if (n > target) {
                 return target;
             }
         }
-        return target;        
+
+        return target;
     }
 };
