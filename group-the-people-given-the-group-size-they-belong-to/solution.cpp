414daa07399ebfb950d3e3462bfa5873037c739e
Abir Akash Bora <143283517+iamabirakash@users.noreply.github.com>
2026-07-25 12:24:14 +0530

    added solution for group-the-people-given-the-group-size-they-belong-to

---
+++ group-the-people-given-the-group-size-they-belong-to/solution.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<vector<int>> groupThePeople(vector<int>& groupSizes) {
+        int n = groupSizes.size();
+        vector<vector<int>> result;
+        unordered_map<int, vector<int>> groups;
+        for (int i = 0; i < n; i++) {
+            int size = groupSizes[i];
+            groups[size].push_back(i);
+            if (groups[size].size() == size) {
+                result.push_back(groups[size]);
+                groups[size].clear();
+            }
+        }
+        
+        return result;
+    }
+};
