5b612c8c309334cb592bd2ab94336139c029525d
Abir Akash Bora <143283517+iamabirakash@users.noreply.github.com>
2026-08-07 10:33:54 +0530

    added solution for smallest-divisible-digit-product-ii

--- smallest-divisible-digit-product-ii/solution.cpp
+++ smallest-divisible-digit-product-ii/solution.cpp
@@ -1,127 +1,56 @@
 class Solution {
-private:
-    bool isInvalid(long long n) {
-        for (int i = 2; i < 10; i++) {
-            while (n % i == 0) {
-                n /= i;
-            }
-        }
-        return n != 1;
-    }
-
-    int minDigitsNeeded(long long t) {
-        if (t <= 1) {
-            return 0;
-        }
-
-        int count = 0;
-
-        for (int d = 9; d >= 2; d--) {
-            while (t % d == 0) {
-                count++;
-                t /= d;
-            }
-        }
-
-        return count;
-    }
-
-    string getMore(long long t) {
-        string sol = "";
-
-        while (t != 1) {
-            int curr = 9;
-
-            while (curr > 1 && (t % curr != 0)) {
-                curr--;
-            }
-
-            sol.push_back(char(curr + '0'));
-            t /= curr;
-        }
-
-        reverse(sol.begin(), sol.end());
-        return sol;
-    }
-
-    long long reduceT(long long t, int d) {
-        for (int f = 9; f >= 2; f--) {
-            while (d % f == 0 && t % f == 0) {
-                t /= f;
-                d /= f;
-            }
-        }
-
-        return t;
-    }
-
 public:
     string smallestNumber(string num, long long t) {
-        int n = num.size();
-
-        if (isInvalid(t)) {
+        long long temp = t;
+        for (int i = 2; i <= 9; i++) {
+            while (temp % i == 0) {
+                temp /= i;
+            }
+        }
+        if (temp > 1) {
             return "-1";
         }
-
-        int zeroIdx = -1;
-
+        int n = num.length();
+        vector<long long> rem(n + 1);
+        rem[0] = t;
+        int pos = n - 1;
         for (int i = 0; i < n; i++) {
             if (num[i] == '0') {
-                zeroIdx = i;
+                pos = i;
                 break;
             }
+            rem[i + 1] = rem[i] / gcd(rem[i], num[i] - '0');
+        }
+        if (rem[n] == 1) {
+            return num;
         }
 
-        if (zeroIdx == -1) {
-            long long tempT = t;
-
-            for (char c : num) {
-                tempT = reduceT(tempT, c - '0');
-            }
-
-            if (tempT == 1) {
-                return num;
-            }
-        }
-
-        int maxPrefix = (zeroIdx == -1) ? n : zeroIdx;
-
-        vector<long long> prefixT(n + 1, t);
-
-        for (int i = 0; i < maxPrefix; i++) {
-            prefixT[i + 1] = reduceT(prefixT[i], num[i] - '0');
-        }
-
-        for (int i = maxPrefix - 1; i >= 0; i--) {
-            int remainingLen = n - 1 - i;
-            int startDigit = (num[i] - '0') + 1;
-
-            for (int d = startDigit; d <= 9; d++) {
-                long long remT = reduceT(prefixT[i], d);
-
-                if (minDigitsNeeded(remT) <= remainingLen) {
-                    string sol = num.substr(0, i);
-                    sol.push_back(char(d + '0'));
-
-                    string suffix = getMore(remT);
-
-                    while ((int)suffix.size() < remainingLen) {
-                        suffix = "1" + suffix;
+        for (int i = pos; i >= 0; i--) {
+            while (++num[i] <= '9') {
+                long long tNow = rem[i] / gcd(rem[i], num[i] - '0');
+                int k = 9;
+                for (int j = n - 1; j > i; j--) {
+                    while (tNow % k) {
+                        k--;
                     }
-
-                    return sol + suffix;
+                    tNow /= k;
+                    num[j] = '0' + k;
+                }
+                if (tNow == 1) {
+                    return num;
                 }
             }
         }
 
-        int reqLen = max(n + 1, minDigitsNeeded(t));
-
-        string sol = getMore(t);
-
-        while ((int)sol.size() < reqLen) {
-            sol = "1" + sol;
+        string ans;
+        for (int i = 9; i > 1; i--) {
+            while (t % i == 0) {
+                ans += '0' + i;
+                t /= i;
+            }
         }
-
-        return sol;
+        ans += string(max(n + 1 - (int)ans.length(), 0), '1');
+        ranges::reverse(ans);
+        return ans;
     }
 };
